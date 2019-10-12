2ac9dda3b70f02f609d6a59b22e623a8c6a3f425
Gabriel Rushin <grushin3@gatech.edu>
2019-10-12 04:14:19 -0400

    some unit tests

---
+++ GroupProject/Words6300/app/src/test/java/edu/gatech/seclass/words6300/GameTest.java
@@ -0,0 +1,41 @@
+package edu.gatech.seclass.words6300;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    // Able to make maxTurns: 0
+    @Test
+    public void isOver() {
+        try{
+            String expected = "GAME OVER";
+            String output;
+            GameSettings is_over_test = new GameSettings(0);
+            Game game_over = new Game(is_over_test);
+
+        } catch (Exception ex){
+            assertEquals( "GAME OVER", ex.getMessage() );
+        }
+    }
+
+    @Test
+    public void getScore() {
+        GameSettings zero_score = new GameSettings(0);
+        Game game_zero_score = new Game(zero_score);
+        int expected = 0;
+
+        assertEquals(0 , game_zero_score.getScore() );
+
+    }
+
+    @Test
+    public void getCurrentTurn() {
+        GameSettings zero_turn_setting = new GameSettings(0);
+        Game game_zero_score = new Game(zero_turn_setting);
+        int expected = 0;
+
+        assertEquals(0,game_zero_score.getCurrentTurn());
+    }
+}
